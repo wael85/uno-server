368c23a6d1be8ee5b0c62b04db55c2719c8f4ff3
Wael Haded <wal@eltronic.dk>
2024-11-03 23:24:18 +0100

    Add server functions

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,96 @@
 import express from 'express';
-import http from 'http';
-import WebSocket, { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws'
+import Game  from './model/game/index'
 
-const app = express();
-const port = 3000;
+const webSocketServer = new WebSocketServer({ port: 9090, path: '/publish' })
+type gameStat = {gameId: number, game: Game}
+type Lobby = { lobbyId: number,maxPlayers:number, score:number ,players: string[]}
+type KeyData = {key: string}
+type Message = {topic: string, message: any}
+interface ClientMap extends Record<string, (data: any, ws: WebSocket) => void> {
+    subscribe(data: KeyData, ws: WebSocket): void
+    unsubscribe(data: KeyData, ws: WebSocket): void
+    send(message: Message, ws: WebSocket): void
+    close(message: any, ws: WebSocket): void
+}
+// const TopicManager = (topic : string , msg:{}) : string =>{
+//     switch(topic){
+//         case 'CreateLooby':
+//             const {score,maxPlayers,userName} = msg
+//             const lobby = CreateLooby(score,maxPlayers,userName)
+            
+//         case 'JoinLooby':
+//             const {lobbyId,userName} = msg
+//             return "OK"
 
-// Create an HTTP server
-const server = http.createServer(app);
+//     }
+//     return "Invalid topic"
+// }
+// const games = new Map<number, Game>();
+// const CreateLooby = (score: number , maxPlayers: number , userName: string): Lobby => {
+//    const  lobbyId =Math.round( Math.random() * 1000)
+//     const players = [userName]
+//     return {lobbyId,maxPlayers,score, players}
 
-// Create a WebSocket server
-const wss = new WebSocketServer({ server });
+// }
+// const JoinLooby = (lobby: Lobby , userName: string): Lobby => {
+//     if(lobby.players.length < lobby.maxPlayers){
+//         lobby.players.push(userName)
+//     }
+//     return lobby
+// }
+// const StartGame = (lobby: Lobby): gameStat => {
+//     const game = new Game(lobby.players, lobby.score);
+//     return {gameId: lobby.lobbyId, game}
+// }
+const clientMap = (): ClientMap => {
+    const clients: Record<string, Set<WebSocket>> = {}
 
-// Listen for WebSocket connections
-wss.on('connection', (ws: WebSocket) => {
- console.log('New client connected');
+    const subscribers = (key: string) => {
+        clients[key] ??= new Set()
+        return clients[key]
+    }
 
- // Handle messages received from clients
- ws.on('message', (message: string) => {
-   console.log(`Received message: ${message}`);
-   // Echo the received message back to the client
-   ws.send(`Server says: ${message}`);
- });
+    const subscribe = ({key}: KeyData, ws: WebSocket) => {
 
- // Handle client disconnections
- ws.on('close', () => {
-   console.log('Client disconnected');
- });
-});
+        subscribers(key).add(ws)
+    }
 
-// Express: Define a simple route
-app.get('/', (req, res) => {
- res.send('Hello from Express!');
-});
+    const unsubscribe = ({key}: KeyData, ws: WebSocket) => subscribers(key).delete(ws)
 
-// Start the server
-server.listen(port, () => {
- console.log(`Server is running on http://localhost:${port}`);
-});
+    const send = ({topic, message}: {topic: string, message: {}}, _: WebSocket) => {
+        for (let ws of subscribers(topic))
+            if (ws.readyState === WebSocket.OPEN) 
+                ws.send(JSON.stringify({topic, message}))
+    }
+
+    const close = (_: unknown, ws: WebSocket) => {
+        if (ws.readyState === WebSocket.OPEN) 
+            ws.close()
+        for(let k in clients)
+            clients[k].delete(ws)
+    }
+
+    return { subscribe, unsubscribe, send, close }
+}
+
+const clients = clientMap()
+
+type Command = { type: string } & Record<string, unknown>
+
+webSocketServer.on('connection', (ws, req) => {
+    ws.on('message', message => {
+        try {
+            const { type, ...params } = JSON.parse(message.toString()) as Command
+            if (clients[type] !== undefined)
+                clients[type](params, ws)
+            else //if (TopicManager(type, params) === "Invalid topic")
+                console.error(`Incorrect message: '${message}' from ${req.socket.remoteAddress} (${req.socket.remoteFamily})`)
+        } catch (e) {
+            console.error(e)
+        }
+    })
+    ws.on('close', () => clients.close({}, ws))
+})
+
+console.log('Pub/Sub server listening on 9090')
